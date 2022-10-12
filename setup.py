e8e6ea834041d71fae6b0ceff31e9747657bae27
Ryan Kortvelesy <rk627@cam.ac.uk>
2022-10-12 15:03:29 +0100

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name="wandb_plotter",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     install_requires=["matplotlib==3.5.0", "pandas", "wandb"],
     author="Ryan Kortvelesy",
